908c0b8059bd88980245c2c59de0b1ec2f65de9e
Tri <quachtri865@gmail.com>
2026-02-05 21:57:31 +0700

    feat: Add AuthenController for handling authentication requests

---
+++ src/main/java/com/group05/TC_LLM_Generator/presentation/controller/AuthenController.java
@@ -0,0 +1,28 @@
+package com.group05.TC_LLM_Generator.presentation.controller;
+
+import com.group05.TC_LLM_Generator.application.dto.AuthResponse;
+import com.group05.TC_LLM_Generator.application.port.in.LoginUseCase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthenController {
+
+    private final LoginUseCase loginUseCase;
+
+    @PostMapping("/login-google")
+    public ResponseEntity<AuthResponse> loginWithGoogle(@RequestBody Map<String, String> request) {
+        String idToken = request.get("idToken");
+        AuthResponse response = loginUseCase.loginWithGoogle(idToken);
+        return ResponseEntity.ok(response);
+    }
+
+}
