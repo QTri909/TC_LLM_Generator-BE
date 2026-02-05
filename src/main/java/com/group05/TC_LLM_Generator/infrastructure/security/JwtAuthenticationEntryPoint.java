bd0dc151e1c98deaa488ddf6b5ae00446219ab6a
Tri <quachtri865@gmail.com>
2026-02-05 21:58:03 +0700

    feat: Implement JwtAuthenticationEntryPoint for unauthorized access handling

---
+++ src/main/java/com/group05/TC_LLM_Generator/infrastructure/security/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,43 @@
+package com.group05.TC_LLM_Generator.infrastructure.security;
+
+import java.io.IOException;
+import java.time.Instant;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ProblemDetail;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import tools.jackson.databind.ObjectMapper;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+            AuthenticationException authException) throws IOException, ServletException {
+
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED); // 401
+
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(
+                HttpStatus.UNAUTHORIZED,
+                authException.getMessage());
+
+        problemDetail.setTitle("Chưa xác thực (Unauthorized)");
+        // problemDetail.setType(URI.create("https://api.yourapp.com/errors/unauthorized"));
+        problemDetail.setProperty("timestamp", Instant.now());
+        problemDetail.setProperty("path", request.getRequestURI());
+
+        objectMapper.writeValue(response.getOutputStream(), problemDetail);
+    }
+}
